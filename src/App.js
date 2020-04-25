41ae2900fca05551389ef1623140c920df5c1306
Shayna Rosado <srosado@pace.edu>
2020-04-24 22:32:32 -0400

    Created start and end nodes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import React from "react";
+import "./App.css";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
 
 function App() {
   return (
     <div className="App">
-        <PathfindingVisualizer></PathfindingVisualizer>
+      <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
 }
--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,16 +1,27 @@
-import React, {Component} from 'react';
-import './Node.css';
+import React, { Component } from "react";
+import "./Node.css";
 
 /**
  * A Node component represents a single square in the grid.
  */
 export default class Node extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return <div className="node"></div>;
-    }
+  render() {
+    const { isFinish, isStart } = this.props;
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : "";
+    return <div className={`node ${extraClassName}`}></div>;
+  }
 }
+
+export const DEFAULT_NODE = {
+  row: 0,
+  col: 0,
+};
